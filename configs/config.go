c1380f0810828a6ca5b4c7a19c7dcbc3b273c594
JavaHutt <alxmer111@gmail.com>
2024-03-31 20:56:51 +0400

    initial commit

---
+++ configs/config.go
@@ -0,0 +1,43 @@
+package configs
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/spf13/viper"
+)
+
+// Config is a configuration object
+type Config struct {
+	Host           string        `yaml:"host"`
+	Port           int           `yaml:"port"`
+	HashExpiration time.Duration `yaml:"hash_expiration"`
+}
+
+// ParseConfig parses the `config.yaml` file in path
+// if path is empty, default config is loaded
+func ParseConfig(path string) (*Config, error) {
+	var config Config
+
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath("./configs")
+
+	if path != "" {
+		viper.AddConfigPath(path)
+	}
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		return nil, fmt.Errorf("error reading config file: %w", err)
+	}
+
+	e := viper.GetDuration("hash_expiration")
+	fmt.Println(e)
+
+	if err = viper.Unmarshal(&config); err != nil {
+		return nil, fmt.Errorf("unable to decode into struct: %w", err)
+	}
+
+	return &config, nil
+}
